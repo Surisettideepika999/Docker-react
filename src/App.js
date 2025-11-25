f418239945e803e4e39282ea4e05d627bcc56e28
Deepika Surisetti <127041514+Surisettideepika999@users.noreply.github.com>
2025-11-25 15:28:22 +0530

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div style={{fontFamily: 'sans-serif', textAlign: 'center', marginTop: '2rem'}}>
-      <h1>Hello, World!</h1>
-      <p>Welcome to your React app.</p>
-      <p>Start editing to see some magic happen :)</p>
-      <p>I am happy</p>
-      <p>Learning Docker is fun!</p>
-      <p>la la ala</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
