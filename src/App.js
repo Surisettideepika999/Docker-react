b71f76d3397f5e30e5043dd3939332dcd8b61da1
Surisettideepika999 <surisettideepika999@gmail.com>
2025-11-25 12:28:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{fontFamily: 'sans-serif', textAlign: 'center', marginTop: '2rem'}}>
+      <h1>Hello, World!</h1>
+      <p>Welcome to your React app.</p>
+      <p>Start editing to see some magic happen :)</p>
+      <p>I am happy</p>
+      <p>Learning Docker is fun!</p>
+      <p>la la ala</p>
+    </div>
+  );
+}
+
+export default App;
